1f3b8504c521d4f9218c122f48cf2065f776fefd
pants007 <qrw992@alumni.ku.dk>
2021-10-13 12:52:36 +0200

    redo the interface to HookedModel, it is now simply called like any other pytorch model
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- src/models.py
+++ src/models.py
@@ -2,6 +2,7 @@ import torch
 import copy
 import torch.nn as nn
 from torchvision import models
+import typing as t
 
 def get_model(name, pretrained = True, num_classes = None, device = 'cuda'):
     if name == 'resnet18':
@@ -56,43 +57,54 @@ class Exposed_model(torch.nn.Module):
         return out_activations
 
 class HookedModel(torch.nn.Module):
-    def __init__(self, model):
+    """Augments a pytorch model with the ability to return activations from specific modules in the model.
+       Intended to be used in the following way: 'retval, activations = model(input, list_of_targets)'"""
+    def __init__(self, model: torch.nn.Module):
         super().__init__()
         self.model = copy.deepcopy(model)
-        self.aux_dict = model.aux_dict
-        self.hooks = {}
-        self.hook_results = {}
-        self.activations = {}
+        self.aux_dict: t.Dict[str, t.Any] = model.aux_dict
+        self._hooks: t.Dict[str, t.Any] = {}
+        self._activations: t.Dict[str, torch.Tensor] = {}
     
     def show_modules(self):
+        """Prints the named modules in the internal model"""
         print(list(self.model.named_modules()))
 
-    def _get_activation(self, name):
-        # the hook signature
+    def _hook_into(self, name):
+        """Returns a hook function meant to be registered with register_forward_hook"""
         def hook(model, input, output):
-            self.activations[name] = output
+            self._activations[name] = output
         return hook
 
-    def register_hook(self, module_string):
-        module = self.model.get_submodule(module_string)
-        self.hooks[module_string] = module.register_forward_hook(self._get_activation(module_string))
+    #TODO in case we want to apply different weights to different activations, perhaps this should return a dictionary instead
+    def _get_activations(self):
+        """Clones the values returned by the forward hooks and returns them as a list"""
+        res = []
+        for val in self._activations.values():
+            res.append(val.clone())
+        self._activations.clear()
+        return res
 
-    def register_hooks(self, module_strings):
-        for module_string in module_strings:
-            module = self.model.get_submodule(module_string)
-            self.hooks[module_string] = module.register_forward_hook(self._get_activation(module_string))
+    def _register_hooks(self, module_names: t.List[str]):
+        """Registers forward hooks on the internal model modules with names in module_names"""
+        for module_name in module_names:
+            module = self.model.get_submodule(module_name)
+            self._hooks[module_name] = module.register_forward_hook(self._hook_into(module_name))
 
-    def unregister_hook(self, name):
-        self.hooks[name].remove()
-        del self.hooks[name]
-
-    def unregister_hooks(self):
-        for hook in self.hooks.values():
-            hook.remove()
-        self.hooks.clear()
+    def _unregister_hooks(self):
+        """Removes any registered hooks and clears the interal hook dictionary"""
+        for module_name in self._hooks.keys():
+            self._hooks[module_name].remove()
+        self._hooks.clear()
             
-    def forward(self, x):
-        return self.model.forward(x)
+    def forward(self, x: torch.Tensor, targets):
+        """"Runs forward on the internal model and returns activations for any model targets specified.
+            targets should be a list of valid module names in the internal model.
+            Module names can be found by calling HookedModel.show_modules()"""
+        self._register_hooks(targets)
+        x = self.model.forward(x)
+        self._unregister_hooks()
+        return x, self._get_activations()
 
 class Dreamnet50(Exposed_model):
     def __init__(self, model):
