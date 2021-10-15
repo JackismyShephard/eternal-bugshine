4cedac938ce8dd8cadb7ac8299fdf156554f356f
pants007 <qrw992@alumni.ku.dk>
2021-10-15 06:03:35 +0200

    add all our models as predefined configs, refactor parts of dream_config into model_config and dataset_config
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,11 +1,12 @@
 import typing as t
-import torch
+import torchvision.transforms
+from .transforms import *
+import multiprocessing as mp
 import numpy as np
 from numpy import typing as npt
 import copy
 import os
 import json
-from ..models import get_model, Exposed_model
 from .custom_types import *
 
 RNG_SEED = 0x1010101
@@ -15,30 +16,27 @@ IMAGENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
 
 BEETLENET_MEAN = np.array([0.8442649, 0.82529384, 0.82333773], dtype=np.float32)
 BEETLENET_STD = np.array([0.28980458, 0.32252666, 0.3240354], dtype=np.float32)
+BEETLENET_AVERAGE_SHAPE = (224, 448)
 
 BEETLENET_PATH = 'data/beetles/images/'
 
-RESNET50 = Exposed_model(get_model('resnet50'), flatten_layer = 'fc')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
+DEFAULT_MODEL_PATH = './models/'
+DEFAULT_METRICS_PATH = './figures/'
 
 DREAM_CONFIG: DreamConfig = {
-    'model': RESNET50,
-    'model_aux_dict': None,
     'out_info': {'fc': None}, #None = whole layer, otherwise specify index as tuple (y,x).
-    'mean': IMAGENET_MEAN,
-    'std': IMAGENET_STD,
-
+    'mean': BEETLENET_MEAN,
+    'std': BEETLENET_STD,
     'input_img_path': None,
     'target_shape': 600,
     'noise': None,
-
     'ratio': 1.8,
     'levels': 4,
     'shift_size': 32,
     'num_iters': 10,
     'lr': 0.09,
-
     'loss_type': 'norm',
     'loss_red': 'mean',
     'norm_type': 'standardize',
@@ -55,25 +53,90 @@ DREAM_CONFIG: DreamConfig = {
     'output_img_path': None,
     'img_overwrite' : False,
     'video_path': None,
-    'video_overwrite' : False,
- 
-    'device': None
+    'video_overwrite' : False
 }
 
-#TODO construct model config for model toolchain simplification
-MODEL_CONFIG = {
-    'model_name': None, #used with load_model
-    'model_architecture': 'resnet34',
-    'dataset_name': None, #should be beetle dataset path by default
-    'dataset_transform': None, #just here for now, will probably be hardcoded in fetch_model or train_model
-    'dataloader_args': {'batch_size':32},
-
-    'optim_args': {'lr': 0.001, 'eps':0.1},
-
+BEETLE_DATASET: DatasetConfig = {
+    'image_folder_path':    './data/beetles/images/',
+    'num_classes':          197,
+    'batch_size':           32,
+    'num_workers':          (mp.cpu_count()//2),
+    'rng_seed':             RNG_SEED,
+    'average_image_shape':  BEETLENET_AVERAGE_SHAPE,
+    'mean':                 BEETLENET_MEAN,
+    'std':                  BEETLENET_STD,
+    'training_data_ratio':  0.8,
+    'validation_data_ratio':0.5,
+    'data_augmentations':   [
+                                RandomVerticalFlip(),
+                                RandomRotation((-3,3), fill=255),
+                                NotStupidRandomResizedCrop(min_scale=0.95, max_scale=1),
+                                RandomizeBackground(cutoff=0.95),
+                                Resize(BEETLENET_AVERAGE_SHAPE),
+                                ToTensor(),
+                                Normalize(BEETLENET_MEAN, BEETLENET_STD)
+                            ]
 }
 
-def get_new_config(param_dict, old_config: DreamConfig = DREAM_CONFIG):
+RESNET50_FULL: ModelConfig = {
+    'model_name':           'resnet50_fullytrained',
+    'model_architecture':   'resnet50',
+    'pretrained':           False,
+    'device':               DEVICE
+}
 
+RESNET50_TRANSFER: ModelConfig = {
+    'model_name':           'resnet50_transferlearned',
+    'model_architecture':   'resnet50',
+    'pretrained':           True,
+    'device':               DEVICE
+}
+
+RESNET34_FULL: ModelConfig = {
+    'model_name':           'resnet34_fullytrained',
+    'model_architecture':   'resnet34',
+    'pretrained':           True,
+    'device':               DEVICE
+}
+
+RESNET34_TRANSFER: ModelConfig = {
+    'model_name':           'resnet34_transferlearned',
+    'model_architecture':   'resnet34',
+    'pretrained':           True,
+    'device':               DEVICE
+}
+
+RESNET18_FULL: ModelConfig = {
+    'model_name':           'resnet18_fullytrained',
+    'model_architecture':   'resnet18',
+    'pretrained':           False,
+    'device':               DEVICE
+}
+
+RESNET18_TRANSFER: ModelConfig = {
+    'model_name':           'resnet18_transferlearned',
+    'model_architecture':   'resnet18',
+    'pretrained':           True,
+    'device':               DEVICE
+}
+
+
+
+DEFAULT_TRAINING: TrainingConfig = {
+    'optim':                None,
+    'optim_args':           {'lr': 0.001, 'eps': 0.1},
+    'criterion':            None,
+    'scheduler':            None,
+    'early_stopping':       None,
+    'early_stopping_args':  {'min_epochs': 200, 'patience': 5, 'min_delta': 0},
+    'train_info':           {'num_epochs': 400, 'trained_epochs': 0, 
+                             'lr_decay': 0.995, 'stopped_early': False,
+                             'test_acc':   0},
+}
+
+def get_new_config(param_dict, old_config: t.Union[DreamConfig, DatasetConfig, None] = None):
+    if old_config is None:
+        raise TypeError('Config must not be None. See src/utils/config.py for default configs')
     config = copy.deepcopy(old_config)
     for (name, param) in param_dict.items():
         config[name] = param
@@ -90,16 +153,57 @@ def extend_path(path, overwrite=False):
             i += 1
         return (root + str(i) + ext)
 
-
-def save_config(config: DreamConfig, path):
+def save_image_metadata(path, dream_config: DreamConfig, model_config: ModelConfig, 
+                        dataset_config: DatasetConfig, training_config: TrainingConfig):
     (root, _) = os.path.splitext(path)
     json_path = root + '.json'
-    new_config = copy.deepcopy(config)
-    for param in ['mean', 'std']:
-        new_config[param] = new_config[param].tolist()
-    for param in ['output_img_path', 'video_path', 'show']:
-        new_config.pop(param)
-    new_config['device'] = new_config['device']
-    new_config['model_aux_dict'] = new_config['model'].aux_dict
+    #convert troublesome dict entries:
+    drc_copy = copy.deepcopy(dream_config)
+    mc_copy = copy.deepcopy(model_config)
+    dc_copy = copy.deepcopy(dataset_config)
+    tr_copy = copy.deepcopy(training_config)
+    device = str(mc_copy['device'])
+    mean = copy.deepcopy(dc_copy['mean'])
+    std = copy.deepcopy(dc_copy['std'])
+    mean = mean.tolist()
+    std = std.tolist()
+    augmentations = [str(aug) for aug in dc_copy['data_augmentations']]
+    new_config = {}
+    new_config.update(model_info=mc_copy)
+    new_config.update(dataset_info=dc_copy)
+    new_config.update(dream_info=drc_copy)
+    new_config.update(train_info=tr_copy)
+    new_config['dataset_info']['data_augmentations'] = augmentations
+    new_config['model_info']['device'] = device
+    new_config['dataset_info']['mean'] = mean
+    new_config['dataset_info']['std'] = std
+    new_config['dream_info']['mean'] = mean
+    new_config['dream_info']['std'] = std
+    
+    with open(json_path, 'w') as json_file:
+        json.dump(new_config, json_file, indent = 4)
+
+def save_training_metadata(path, model_config, dataset_config, training_config):
+    (root, _) = os.path.splitext(path)
+    json_path = root + '.json'
+    #convert troublesome dict entries:
+    mc_copy = copy.deepcopy(model_config)
+    dc_copy = copy.deepcopy(dataset_config)
+    tr_copy = copy.deepcopy(training_config)
+    device = str(mc_copy['device'])
+    mean = copy.deepcopy(dc_copy['mean'])
+    std = copy.deepcopy(dc_copy['std'])
+    mean = mean.tolist()
+    std = std.tolist()
+    augmentations = [str(aug) for aug in dc_copy['data_augmentations']]
+    new_config = {}
+    new_config.update(model_info=mc_copy)
+    new_config.update(dataset_info=dc_copy)
+    new_config.update(train_info=tr_copy)
+    new_config['dataset_info']['data_augmentations'] = augmentations
+    new_config['model_info']['device'] = device
+    new_config['dataset_info']['mean'] = mean
+    new_config['dataset_info']['std'] = std
+    
     with open(json_path, 'w') as json_file:
         json.dump(new_config, json_file, indent = 4)
