6aa5a9482d7c9226826948efaf877d8b6acdebfe
Christian Troelsen <jackismyshephard@gmail.com>
2021-10-18 18:41:37 +0200

    add todos and questions

--- src/utils/training.py
+++ src/utils/training.py
@@ -56,6 +56,7 @@ class EarlyStopping():
 
 
 #IMPLEMENT rolling average
+# TODO incorcorate model into model_config and data_loaders, dataset_sizes into dataset_config or a similar dicts
 def fit(model, data_loaders, dataset_sizes,
         model_config: ModelConfig, training_config: TrainingConfig, dataset_config: DatasetConfig, 
         clear='terminal', plot=False, save_interval=25):
@@ -73,22 +74,27 @@ def fit(model, data_loaders, dataset_sizes,
     val_loss_avg, val_acc_avg = [], []
     epochs = []
 
+    # TODO parametrize this value
     # As for now it is defined here 
     rolling_avg_len = 50
 
     
-
+    # TODO since we already have criterion in training_config why not just add cross_entropy as default there?
     criterion = torch.nn.CrossEntropyLoss()
+
     optim_args = training_config['optim_args']
+    # TODO the same could be said about the optimizer.
     optimizer = torch.optim.Adam(model.parameters(), lr=optim_args['lr'], eps=optim_args['eps'])
 
     lr_decay = training_config['train_info']['lr_decay']
+    # TODO also consider the same for scheduler
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=lr_decay)
 
     es_args = training_config['early_stopping_args']
+    # TODO The same
     early_stopping = EarlyStopping(min_epochs = es_args['min_epochs'], 
                         patience=es_args['patience'], min_delta=es_args['min_delta'])
-
+    # TODO add these as parameters in eg training_config
     metrics_path = DEFAULT_METRICS_PATH
     model_path   = DEFAULT_MODEL_PATH
     
@@ -121,7 +127,12 @@ def fit(model, data_loaders, dataset_sizes,
 
                     # forward
                     # track history if only in train
+
                     with torch.set_grad_enabled(phase == 'train'):
+                    
+                        # TODO make this part of the code a subprocedure that can switched in and out, eg give it as a parameter
+                        # So that we can later apply training to models with a more complicated output format as well as using different
+                        # metrics
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
@@ -132,10 +143,13 @@ def fit(model, data_loaders, dataset_sizes,
                             optimizer.step()
 
                     # statistics
+                # TODO if we implement the above TODO then this code might also need to be changed depending on which metrics and model are used
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
                 epoch_loss = running_loss / dataset_sizes[phase]
                 epoch_acc = (running_corrects.double()).item() / dataset_sizes[phase]
+
+
                 if phase == 'train':
                     train_loss.append(epoch_loss)
                     train_acc.append(epoch_acc)
@@ -150,12 +164,15 @@ def fit(model, data_loaders, dataset_sizes,
                     #rolling average
                     val_loss_avg.append(np.mean(val_loss[-rolling_avg_len:len(val_loss)]))
                     val_acc_avg.append(np.mean(val_acc[-rolling_avg_len:len(val_acc)]))
+                    
+                    # TODO consider choosing final model based on loss and not accuracy
 
                     if epoch_acc > best_acc:
                         best_acc = epoch_acc
                         best_model_wts = copy.deepcopy(model.state_dict())
                         best_model_epochs = int(epoch)
                     early_stopping(epoch_loss)
+            # TODO this clear == notebook might need to be changed if we are using widgets
             if clear == 'notebook':
                 clear_output(wait=True)
             elif clear == 'terminal':
@@ -170,14 +187,17 @@ def fit(model, data_loaders, dataset_sizes,
             metrics = np.array([epochs, train_loss, train_acc,
                                     val_loss, val_acc])
             if plot == True:
+                # QUESTION why not just save all metrics in "metrics" to begin with?
                 plotting_metrics = np.array([epochs, train_loss, train_acc,
                                     val_loss, val_acc, train_loss_avg,
                                      train_acc_avg, val_loss_avg, val_acc_avg ])
+                # TODO the save path should probably be metrics_path + model_name
                 plot_metrics(plotting_metrics, metrics_path)
             scheduler.step()
             if epoch % save_interval == 0:
                 temp_state_dict = copy.deepcopy(model.state_dict())
                 model.load_state_dict(best_model_wts)
+                # TODO maybe just pass the model state dict to save rather than the model itself?
                 training_config['train_info']['trained_epochs'] = best_model_epochs
                 save_model(model, model_path+model_config['model_name'], optim=None,dataloaders=data_loaders, train_metrics=metrics)
                 save_training_metadata(model_path+model_config['model_name'], model_config, dataset_config, training_config)
@@ -191,10 +211,11 @@ def fit(model, data_loaders, dataset_sizes,
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
+    # TODO maybe also print best loss
     print('Best val Acc: {:4f}'.format(best_acc))
     # load best model weights
     model.load_state_dict(best_model_wts)
-    
+    # QUESTION any way of avoiding the code below?
     training_config['train_info']['trained_epochs'] = best_model_epochs
     save_model(model, model_path+model_config['model_name'], optim=None,dataloaders=data_loaders, train_metrics=metrics)
     save_training_metadata(model_path+model_config['model_name'], model_config, dataset_config, training_config)
